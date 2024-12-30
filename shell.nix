ccec22af5f9feb18bd72ce63a4a68427b7531a40
CommanderKeynes <andrewjackson947@gmail.coma>
2024-12-29 20:04:28 -0600

    Got uncrustify to work

--- shell.nix
+++ shell.nix
@@ -19,22 +19,30 @@ in pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = [
         pkgs.pkg-config
+        pkgs.cmake
+        pkgs.gcc
     ];
     buildInputs = [
 	pkgs.cirrus-cli
         pkgs.automake
+        pkgs.autoconf
         pkgs.c-ares
         pkgs.curl
         pkgs.cacert
-        pkgs.cmake
         pkgs.libevent
         pkgs.libtool
         pkgs.openssl
+        pkgs.uncrustify
         pkgs.pam
+        pkgs.which
+        pkgs.git
         pkgs.postgresql
         pkgs.python39
         pkgs.systemd
         pkgs.valgrind
         pythonPackages
     ];
+    shellHook = ''
+        cp ${pkgs.uncrustify}/bin/uncrustify ./uncrustify
+    '';
 }
